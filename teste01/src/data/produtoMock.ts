518aead74299f55dadd7684d866a0cf8ac38456f
Valadares24 <valadareslf0@gmail.com>
2026-08-17 17:28:45 -0300

    feat: mock de produtos (3 prod)

---
+++ teste01/src/data/produtoMock.ts
@@ -0,0 +1,28 @@
+import {Produto} from "../types";
+
+export const ProdutosMock: Produto[] = [
+{
+    id: '1',
+    nome: 'Arroz Integral 1kg',
+    preco: 8.5,
+    descricao: 'Arroz integral rico em fibras e nutrientes.',
+    categoria: 'Alimentos',
+    imgUrl: 'https://placehold.co/150x150/png',
+    },
+    {
+    id: '2',
+    nome: 'Feijão Preto 1kg',
+    preco: 7.2,
+    descricao: 'Feijão preto tipo 1 de alta qualidade.',
+    categoria: 'Alimentos',
+    imgUrl: 'https://placehold.co/150x150/png',
+  },
+  {
+    id: '3',
+    nome: 'Azeite Extra Virgem 500ml',
+    preco: 35.9,
+    descricao: 'Azeite de oliva extra virgem importado.',
+    categoria: 'Mercearia',
+    imgUrl: 'https://placehold.co/150x150/png',
+  },
+];
